377be8477571056ecfbcb7b6a348a6148bffcbe9
jerrysu5379 <jerrysu5379@gmail.com>
2024-03-21 08:02:10 +0800

    up

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
 
-int main(){
-    int num;
+void sort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
 
+int main() {
+    char *str = malloc(sizeof(long long));
+    scanf("%[^\n]", str);
+    int top3[3] = {INT_MIN, INT_MIN, INT_MIN};
+    char *token = strtok(str, " ");
+    while(token != NULL) {
+        int num = atoi(token);
+        if(num > top3[2]) {
+            top3[2] = num;
+            sort(top3, 3);
+        }
+        token = strtok(NULL, " ");
+    }
+    printf("%d", top3[2]);
+    return 0;
 }
